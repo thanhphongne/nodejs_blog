a8198d7cf5bfd6efc6331f7b72a80d32cc595fff
thanhphong <btphongc3tl@gmail.com>
2021-10-29 15:59:23 +0700

    ket noi mongodb

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,12 +1,22 @@
+const Course = require('../models/Course');
+
 class SiteController {
-  // [GET] /
-  index(req, res) {
-    res.render('home');
-  }
-  // [GET] /search
-  search(req, res) {
-    res.render('search');
-  }
+    // [GET] /
+    index(req, res) {
+        // res.render('home');
+
+        Course.find({}, function (err, courses) {
+            if (!err) {
+                res.json(courses);
+            } else {
+                res.status(400).json({ error: 'ERROR!!!' });
+            }
+        });
+    }
+    // [GET] /search
+    search(req, res) {
+        res.render('search');
+    }
 }
 
 module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ const exp = require('constants');
 const app = express();
 const port = 3000;
 const route = require('./routes');
+const db = require('./config/db');
+
+//connect toi db
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
-  express.urlencoded({
-    extended: true,
-  }),
+    express.urlencoded({
+        extended: true,
+    }),
 );
 app.use(express.json());
 //http logger
@@ -20,18 +24,18 @@ app.use(express.json());
 
 //template engine
 app.engine(
-  'hbs',
-  handlebars({
-    extname: '.hbs',
-  }),
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
 route(app);
 
 //localhost: 127.0.0.1
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
